1cc1db7fea559c53b44574ab7e480281c71e3137
nazibur-175 <nazibur175@gmail.com>
2023-04-27 11:37:56 +0600

    Administrator complete

--- hotelManagement.cpp
+++ hotelManagement.cpp
@@ -1,24 +1,24 @@
-#include<bits/stdc++.h>
-#include<conio.h>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <conio.h>
+#include <cstdio>
 using namespace std;
 
-
-
-
+void manageRoom();
 
 class administrator
 {
 public:
     void adminMenu()
     {
-        ofstream reg("database.txt", ios::app);
+        ofstream reg("username&password.txt", ios::app);
+        // ofstream reg("username&password.txt");
         system("cls");
         cout << " ********Admin Menu******\n";
         cout << "1.Login";
-        cout << "\n2.Forgot Credentials"<<endl;
-        cout<<"3.Main Menu";
+        cout << "\n2.Forgot Credentials" << endl;
+        cout << "3.Main Menu";
         cout << "\nEnter your choice:";
-
 
         int choice;
         cin >> choice;
@@ -31,10 +31,10 @@ public:
             forgot();
             break;
         case 3:
-            return ;
+            return;
         default:
         {
-           cout << "Your have a wrong choice. Press a key to try again.";
+            cout << "Your have a wrong choice. Press a key to try again.";
             getch();
             adminMenu();
         }
@@ -50,7 +50,7 @@ public:
         cin >> user;
         cout << "Enter the password :";
         cin >> pass;
-        ifstream input("database.txt");
+        ifstream input("username&password.txt");
         while (input >> u >> p)
         {
             if (u == user && p == pass)
@@ -61,10 +61,11 @@ public:
         input.close();
         if (exist == 1)
         {
-            cout << "Hello " << user << "\n We're glad that you're here \n";
-            getch();
+            // cout << "Hello " << user << "\n We're glad that you're here \n";
+            // getch();
+            manageRoom();
             // main();
-            adminMenu();
+            // adminMenu();
         }
         else
         {
@@ -89,7 +90,7 @@ public:
         {
             int ex = 0;
             string searchuser, su, sp;
-            ifstream searchu("database.txt");
+            ifstream searchu("username&password.txt");
             cout << "Enter your remembered username :";
             cin >> searchuser;
 
@@ -122,7 +123,7 @@ public:
         {
             int exi = 0;
             string searchpass, su2, sp2;
-            ifstream searchp("database.txt");
+            ifstream searchp("username&password.txt");
             cout << "Enter the remembered password :";
             cin >> searchpass;
 
@@ -165,61 +166,271 @@ public:
     }
 };
 
+class roomManagement
+{
+public:
+    string roomNo, bedNum, ac, rent, status;
+    void addRoom()
+    {
+        // cout<<"Add Room"<<endl;
+        // getch();
+        system("cls");
+        cout << "1.Vip Room" << endl;
+        cout << "2.Normal Room" << endl;
+        cout << "3.Back" << endl;
+        int choice;
+        cin >> choice;
+        if (choice == 1)
+        {
+            // cout<<"VIP"<<endl;
+            // getch();
+            ofstream vip("viproom.txt", ios::app);
+            cout << "Please enter Room No" << endl;
+            cin >> roomNo;
+            cout << "Please enter Number of bed" << endl;
+            cin >> bedNum;
+            cout << "Please enter Ac or Non-Ac" << endl;
+            cin >> ac;
+            cout << "Please enter Rent" << endl;
+            cin >> rent;
+            // cout<<"Please enter status"<<endl;
+            // cin>>status;
+            status = "0";
+            vip << roomNo << " " << bedNum << " " << ac << " " << rent << " " << status << endl;
+            // while(cin>>roomNo>>bedNum>>ac>>rent>>status)
+            // {
+            //     vip<<roomNo<<" "<<bedNum<<" "<<ac<<" "<<rent<<" "<<status<<endl;
+            // }
+        }
+        else if (choice == 2)
+        {
+            ofstream vip("normalroom.txt", ios::app);
+            // string roomNo,bedNum,ac,rent,status;
+            cout << "Please enter Room No" << endl;
+            cin >> roomNo;
+            cout << "Please enter Number of bed" << endl;
+            cin >> bedNum;
+            cout << "Please enter Ac or Non-Ac" << endl;
+            cin >> ac;
+            cout << "Please enter Rent" << endl;
+            cin >> rent;
+            // cout<<"Please enter status"<<endl;
+            // cin>>status;
+            status = "0";
+            vip << roomNo << " " << bedNum << " " << ac << " " << rent << " " << status << endl;
+        }
+        else if (choice == 3)
+        {
+            return;
+        }
+        else
+        {
+            cout << "Your have a wrong choice. Press a key to try again." << endl;
+            getch();
+            addRoom();
+        }
+    }
+    void displayRoom()
+    {
+        system("cls");
+        cout << "1.Vip Room" << endl;
+        cout << "2.Normal Room" << endl;
+        cout << "3.Back" << endl;
+        int choice;
+        cin >> choice;
+        if (choice == 1)
+        {
+            ifstream vip("viproom.txt", ios::app);
+            cout << "show all room:\n";
+            cout << "\n+---------+---------------+--------------+------------+-----------+";
+            cout << "\n| Room No.| Number of bed | Ac or Non Ac |    Rent    |   Status  |";
+            cout << "\n+---------+---------------+--------------+------------+-----------+\n";
+            while (vip >> roomNo >> bedNum >> ac >> rent >> status)
+            {
+                cout << "   " << roomNo << "\t         " << bedNum << "\t        " << ac << "\t      " << rent << "\t     " << status << endl;
+            }
+        }
+        else if (choice == 2)
+        {
+            ifstream normal("normalroom.txt", ios::app);
+            cout << "show all room:\n";
+            cout << "\n+---------+---------------+--------------+------------+-----------+";
+            cout << "\n| Room No.| Number of bed | Ac or Non Ac |    Rent    |   Status  |";
+            cout << "\n+---------+---------------+--------------+------------+-----------+\n";
+            while (normal >> roomNo >> bedNum >> ac >> rent >> status)
+            {
+                cout << "   " << roomNo << "\t         " << bedNum << "\t        " << ac << "\t      " << rent << "\t     " << status << endl;
+            }
+        }
+        else
+        {
+            return;
+        }
+        getch();
+    }
+
+    void copyvip()
+    {
+
+        ifstream viptemp("viptemp.txt", ios::app);
+        ofstream viproom("viproom.txt", ios::out);
+        while (viptemp >> roomNo >> bedNum >> ac >> rent >> status)
+        {
+                viproom << roomNo << " " << bedNum << " " << ac << " " << rent << " " << status << endl;
+        }
+    }
+
+    void copynormal()
+    {
+
+        ifstream normaltemp("normaltemp.txt", ios::app);
+        ofstream normalroom("normalroom.txt", ios::out);
+        while (normaltemp >> roomNo >> bedNum >> ac >> rent >> status)
+        {
+                normalroom << roomNo << " " << bedNum << " " << ac << " " << rent << " " << status << endl;
+        }
+    }
+
+    void modifyRoom()
+    {
+        system("cls");
+        cout << "1.Vip Room" << endl;
+        cout << "2.Normal Room" << endl;
+        cout << "3.Back" << endl;
+        int choice;
+        cin >> choice;
+        string rmNo;
+        cout << "Enter room no:";
+        cin >> rmNo;
+        if (choice == 1)
+        {
+            // read mood
+            fstream vip;
+            vip.open("viproom.txt", ios::in);
+            // write mood
+            fstream viptemp;
+            viptemp.open("viptemp.txt", ios::out);
+
+            while (vip >> roomNo >> bedNum >> ac >> rent >> status)
+            {
+                if (rmNo == roomNo)
+                {
+                    
+                    cout << "Please enter Number of bed" << endl;
+                    cin >> bedNum;
+                    cout << "Please enter Ac or Non-Ac" << endl;
+                    cin >> ac;
+                    cout << "Please enter Rent" << endl;
+                    cin >> rent;
+                    cout << "Please enter status" << endl;
+                    cin >> status;
+                    // status = "0";
+                    viptemp << roomNo << " " << bedNum << " " << ac << " " << rent << " " << status << endl;
+                }
+                else
+                {
+                    viptemp << roomNo << " " << bedNum << " " << ac << " " << rent << " " << status << endl;
+                }
+            }
+            copyvip();
+            // vip.close();
+            // viptemp.close();
+
+        }
 
 
 
+        else if (choice == 2)
+        {
+            // read mood
+            fstream normal;
+            normal.open("normalroom.txt", ios::in);
+            // write mood
+            fstream normaltemp;
+            normaltemp.open("normaltemp.txt", ios::out);
+
+            while (normal >> roomNo >> bedNum >> ac >> rent >> status)
+            {
+                if (rmNo == roomNo)
+                {
+                    
+                    cout << "Please enter Number of bed" << endl;
+                    cin >> bedNum;
+                    cout << "Please enter Ac or Non-Ac" << endl;
+                    cin >> ac;
+                    cout << "Please enter Rent" << endl;
+                    cin >> rent;
+                    cout << "Please enter status" << endl;
+                    cin >> status;
+                    // status = "0";
+                    normaltemp << roomNo << " " << bedNum << " " << ac << " " << rent << " " << status << endl;
+                }
+                else
+                {
+                    normaltemp << roomNo << " " << bedNum << " " << ac << " " << rent << " " << status << endl;
+                }
+            }
+            copynormal();
+            // vip.close();
+            // viptemp.close();
+        }
+        else
+        {
+            return;
+        }
+        
+        getch();
+    }
+};
 
 class customer
 {
 public:
     void check()
     {
-        cout<<"Customers"<<endl;
+        cout << "Customers" << endl;
         getch();
         return;
     }
 };
 
-
-
-
 void mainMenu()
 {
-    cout<<"1.Administrator"<<endl;
-    cout<<"2.Customer"<<endl;
-    cout<<"3.End the program"<<endl;
+    cout << "1.Administrator" << endl;
+    cout << "2.Customer" << endl;
+    cout << "3.End the program" << endl;
 }
-
-
-
 
 int main()
 {
-    //Homepage
-    cout<<"      \n\t\t\t--------------------------------";
-    cout<<"      \n\t\t\t| HOTEL MANAGEMENT PROJECT      |";
-    cout<<"      \n\t\t\t--------------------------------";
-    cout<<"      \n\n\t\t\t Developed By:   Nazibur Rahman(IT-21017)\n";
-    cout<<"      \t\t\t\t\t Mahmudul Hasan(IT-21019)\t\t\t\t\t   ";
-    cout<<"      \n\n\t\t\t submitted to :  S.M. Shamim";
-    cout<<"      \n\t\t\t\t\t Assistant Professor  ";
-    cout<<"      \n\t\t\t\t\t ICT,MBSTU  ";
-    cout<<"      \n\n\n\n\n\n\n\t\t\tPress any key to enter the main program!!\n";
+    // Homepage
+    cout << "      \n\t\t\t--------------------------------";
+    cout << "      \n\t\t\t| HOTEL MANAGEMENT PROJECT      |";
+    cout << "      \n\t\t\t--------------------------------";
+    cout << "      \n\n\t\t\t Developed By:   Nazibur Rahman(IT-21017)\n";
+    cout << "      \t\t\t\t\t Mahmudul Hasan(IT-21019)\t\t\t\t\t   ";
+    cout << "      \n\n\t\t\t submitted to :  S.M. Shamim";
+    cout << "      \n\t\t\t\t\t Assistant Professor  ";
+    cout << "      \n\t\t\t\t\t ICT,MBSTU  ";
+    cout << "      \n\n\n\n\n\n\n\t\t\tPress any key to enter the main program!!\n";
     getch();
 
-    //Object creation
+    // Object creation
     administrator admin;
     customer custom;
 
-    //Main Menu
+    // roomManagement ob;
+    // ob.modifyRoom();
+
+    // Main Menu
     int choice;
     do
     {
         system("cls");
         mainMenu();
-        cout<<"Please enter you choice :"<<endl;
-        cin>>choice;
-        switch(choice)
+        cout << "Please enter you choice :" << endl;
+        cin >> choice;
+        switch (choice)
         {
         case 1:
             admin.adminMenu();
@@ -229,13 +440,42 @@ int main()
             break;
         case 3:
             return 0;
-        default :
+        default:
             cout << "Your have a wrong choice. Press a key to try again.";
             getch();
-            //break;
-
+            // break;
         }
-    }
-    while(choice !=3);
+    } while (choice != 3);
+}
 
+void manageRoom()
+{
+    roomManagement rm;
+
+    int option;
+    do
+    {
+        system("cls");
+        cout << "1.Add room\n2.Display all room\n3.Modify room\n4.Back to main manu\nEnter your option:";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            rm.addRoom();
+            break;
+        case 2:
+            rm.displayRoom();
+            break;
+        case 3:
+        {
+            rm.modifyRoom();
+            break;
+        }
+        case 4:
+            break;
+        default:
+            cout << "\nPlease Enter correct option\n";
+            break;
+        }
+    } while (option != 4);
 }
